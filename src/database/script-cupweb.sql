e47e879748fa59fc30a934841777e8e5bda5f756
Davissantan4 <davi.ssilva@sptech.school>
2025-05-19 23:04:02 -0300

    Melhorias no banco de dados

--- src/database/script-cupweb.sql
+++ src/database/script-cupweb.sql
@@ -3,26 +3,39 @@ use cupweb;
 
 create table Usuario(
 id int primary key auto_increment,
-nome varchar(50) not null,
-email varchar(100) not null,
-senha varchar(100) not null,
-unique ix_email (email)
+nome varchar(50),
+email varchar(100),
+senha varchar(100)
 );
 
 create table Quiz(
 id int primary key auto_increment,
-qtdQuestoes int not null
+descricao varchar(255)
 );
 
-create table Tentativa_quiz(
-id int primary key auto_increment,
-fkUsuario int not null,
+create table Quiz_questao(
+idQuestao int primary key auto_increment,
 fkQuiz int not null,
-tentativa int not null,
-acertos int not null,
-erros int not null,
-porcentagemAcertos decimal(5,2),
+enunciado varchar(255),
+resposta_correta varchar(150),
+alternativa_correta char(1),
+foreign key(fkQuiz) references Quiz(id)
+);
+
+create table Quiz_resposta(
+idResposta int primary key auto_increment,
+fkQuestao int not null,
+resposta varchar(150),
+alternativa char(1),
+foreign key(fkQuestao) references Quiz_questao(idQuestao)
+);
+
+create table Usuario_quiz_resposta(
+idTentativa int not null,
+fkUsuario int not null,
+fkResposta int not null,
+acertou boolean,
 foreign key(fkUsuario) references Usuario(id),
-foreign key(fkQUiz) references Quiz(id),
-unique ix_usuario_quiz_tentativa (fkUsuario, fkQuiz, tentativa)
+foreign key(fkResposta) references Quiz_resposta(idResposta),
+primary key(idTentativa, fkUsuario, fkResposta)
 );
